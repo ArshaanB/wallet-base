100652c32f753f97921ff7491c59b9e830479f52
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-10-12 22:59:44 -0400

    Add createUser function for creating a new user in db

--- app/utils/database.tsx
+++ app/utils/database.tsx
@@ -1,3 +1,14 @@
-export function createDatabase(name: string) {
-  console.log(`createDatabase for ${name}`);
+// If it doesn't work in the future: npx prisma generate
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function createUser(name?: string) {
+  const newUser = await prisma.user.create({
+    data: {
+      name: name
+    }
+  });
+
+  return newUser;
 }
